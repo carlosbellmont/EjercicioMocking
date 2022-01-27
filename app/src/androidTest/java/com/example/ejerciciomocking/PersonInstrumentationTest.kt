d7817c2ace1d5be47e86ee53384449f33e3c0018
cbellmont <carlos.bellmont@gmail.com>
2022-01-27 16:56:40 +0100

    androidTestImplementation added

---
+++ app/src/androidTest/java/com/example/ejerciciomocking/PersonInstrumentationTest.kt
@@ -0,0 +1,42 @@
+package com.example.ejerciciomocking
+
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class PersonInstrumentationTest {
+
+    private lateinit var SUT : Persona
+
+    @Mock
+    private lateinit var repositorioPersona: RepositorioPersona
+
+    @Before
+    fun setUp(){
+        SUT = Persona("Carlos")
+    }
+
+    @Test
+    fun obtenerAmigos_NoMock() {
+        Assert.assertEquals(SUT.obtenerMejorAmigo(RepositorioPersona()), "Juan")
+    }
+
+    @Test
+    fun obtenerAmigos_MockRodolfo() {
+        Mockito.`when`(repositorioPersona.obtenerMejorAmigo(SUT)).thenReturn("Rodolfo")
+        Assert.assertEquals(SUT.obtenerMejorAmigo(repositorioPersona), "Rodolfo")
+    }
+
+    @Test
+    fun obtenerAmigos_MockNull() {
+        Mockito.`when`(repositorioPersona.obtenerMejorAmigo(SUT)).thenReturn(null)
+        Assert.assertEquals(SUT.obtenerMejorAmigo(repositorioPersona), null)
+    }
+}
